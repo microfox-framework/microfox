d1e686b0765e67e7c9fffc661dbcf86520573088
Mahdi <mah454@mail.moke.ir>
2025-09-09 03:55:48 +0330

    improv test

--- microfox-http/src/test/java/com/sample/HttpTest.java
+++ microfox-http/src/test/java/com/sample/HttpTest.java
@@ -3,14 +3,17 @@ package com.sample;
 import com.sample.exception.MyExceptionMapper;
 import com.sample.resource.EchoEndpoint;
 import com.sample.resource.EchoRoute;
+import com.sample.sse.SseTask;
 import ir.moke.microfox.MicroFox;
 import ir.moke.microfox.api.http.Method;
 import ir.moke.microfox.api.http.Request;
 import ir.moke.microfox.api.http.Response;
+import ir.moke.microfox.api.http.sse.SseObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalTime;
+import java.util.Timer;
 
 public class HttpTest {
     private static final Logger logger = LoggerFactory.getLogger(HttpTest.class);
@@ -20,11 +23,22 @@ public class HttpTest {
         MicroFox.httpFilter("/api/*", HttpTest::simpleFilter);
         MicroFox.httpRouter("/api/echo", Method.GET, new EchoRoute());
         MicroFox.websocket(EchoEndpoint.class);
+
+
+        /*
+        * Install httpie package
+        * run this command :
+        * http --stream http://localhost:8080/api/sse 'Accept: text/event-stream'
+        * */
+        MicroFox.sseRegister("sse-test", "/api/sse");
+        MicroFox.ssePublisher("sse-test", () -> new SseObject("Hello"));
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(new SseTask(), 2000, 3000);
     }
 
     private static boolean simpleFilter(Request req, Response resp) {
         logger.info("Receive message : {}", LocalTime.now());
         resp.body("Filter Executed\n");
-        return false;
+        return true;
     }
 }
---
+++ microfox-http/src/test/java/com/sample/sse/SseTask.java
@@ -0,0 +1,18 @@
+package com.sample.sse;
+
+import ir.moke.microfox.api.http.sse.SseObject;
+import ir.moke.microfox.http.ResourceHolder;
+
+import java.time.LocalDateTime;
+import java.util.TimerTask;
+import java.util.concurrent.SubmissionPublisher;
+
+public class SseTask extends TimerTask {
+    @Override
+    public void run() {
+        SubmissionPublisher<SseObject> submissionPublisher = ResourceHolder.getSseByIdentity("sse-test");
+        if (submissionPublisher != null) {
+            submissionPublisher.submit(new SseObject("Hello: " + LocalDateTime.now()));
+        }
+    }
+}
