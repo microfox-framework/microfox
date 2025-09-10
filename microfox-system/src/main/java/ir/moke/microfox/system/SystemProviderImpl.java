aa695838753a7c5b723daf6ecffb7cffa09ea40c
Mahdi <mah454@mail.moke.ir>
2025-09-11 01:03:07 +0330

    improvement and fix system module

--- microfox-system/src/main/java/ir/moke/microfox/system/SystemProviderImpl.java
+++ microfox-system/src/main/java/ir/moke/microfox/system/SystemProviderImpl.java
@@ -1,12 +1,17 @@
 package ir.moke.microfox.system;
 
+import ir.moke.microfox.MicroFox;
+import ir.moke.microfox.MicrofoxEnvironment;
 import ir.moke.microfox.api.system.SystemProvider;
+import ir.moke.microfox.utils.HttpClientConfig;
 
-import java.time.LocalTime;
-import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class SystemProviderImpl implements SystemProvider {
-    private static final Timer timer = new Timer("microfox-system-task", true);
+    private static final ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+    private static final SystemDTO SYSTEM_DTO = new SystemDTO();
 
     static {
         onShutdown();
@@ -14,12 +19,49 @@ public class SystemProviderImpl implements SystemProvider {
 
     @Override
     public void activate() {
-        LocalTime now = LocalTime.now();
-        long delay = 10 - (now.getSecond() % 10) * 1000;
-        timer.scheduleAtFixedRate(new StatisticTask(), delay, 10000);
+        ses.scheduleAtFixedRate(this::run, 0, 30, TimeUnit.SECONDS);
+    }
+
+    public void run() {
+        generateSystemDTO();
+        send();
+    }
+
+    public static void send() {
+        HttpClientConfig config = new HttpClientConfig.Builder()
+                .setBaseUri(MicrofoxEnvironment.getEnv("microfox.admin.base-url"))
+                .build();
+        MicroFoxAdminAPI microFoxAdminAPI = MicroFox.httpClient(config, MicroFoxAdminAPI.class);
+        microFoxAdminAPI.statistics(SYSTEM_DTO);
+    }
+
+    private static void generateSystemDTO() {
+        SYSTEM_DTO.setPid(SystemInformation.pid());
+        SYSTEM_DTO.setThreads(SystemInformation.threads());
+        SYSTEM_DTO.setContainer(SystemInformation.isContainer());
+        SYSTEM_DTO.setHostname(SystemInformation.hostname());
+        SYSTEM_DTO.setEnvironments(SystemInformation.environments());
+        SYSTEM_DTO.setNameservers(SystemInformation.dnsNameServers());
+        SYSTEM_DTO.setHosts(SystemInformation.hosts());
+        SYSTEM_DTO.setJvmVendor(SystemInformation.jvmVendor());
+        SYSTEM_DTO.setJvmVersion(SystemInformation.jvmVersion());
+        SYSTEM_DTO.setJvmMaxHeapSize(SystemInformation.jvmMaxHeapSize());
+        SYSTEM_DTO.setJvmTotalHeapSize(SystemInformation.jvmTotalHeapSize());
+        SYSTEM_DTO.setJvmUsedHeapSize(SystemInformation.jvmUsedHeapSize());
+        SYSTEM_DTO.setUlimits(SystemInformation.ulimit());
+        SYSTEM_DTO.setModInfos(SystemInformation.lsmod());
+        SYSTEM_DTO.setRoutes(SystemInformation.routes());
+        SYSTEM_DTO.setNetstat(SystemInformation.netstat());
+        SYSTEM_DTO.setEthernets(SystemInformation.ethernets());
+        SYSTEM_DTO.setDisks(SystemInformation.disks());
+        SYSTEM_DTO.setPartitions(SystemInformation.partitions());
+        SYSTEM_DTO.setDevices(SystemInformation.devices());
+        SYSTEM_DTO.setDateTime(SystemInformation.zonedDateTime());
+        SYSTEM_DTO.setSysctl(SystemInformation.sysctl());
+        SYSTEM_DTO.setClassInstance(SystemInformation.instanceList());
     }
 
     private static void onShutdown() {
-        Runtime.getRuntime().addShutdownHook(new Thread(timer::cancel));
+        Runtime.getRuntime().addShutdownHook(new Thread(ses::shutdown));
     }
 }
