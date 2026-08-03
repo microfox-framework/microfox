5a1f366a554e77dc3becda3ba0c113e1cedd4410
m.sheikhhosseini <m.sheikhhosseini@sadad.co.ir>
2026-08-03 11:33:05 +0330

    remove unusable files

--- microfox-system/src/main/java/ir/moke/microfox/system/ClassInstance.java
+++
@@ -1,5 +0,0 @@
-package ir.moke.microfox.system;
-
-public record ClassInstance(long instances, long bytes, String name) {
-
-}
--- microfox-system/src/main/java/ir/moke/microfox/system/MicroFoxAdminAPI.java
+++
@@ -1,8 +0,0 @@
-//package ir.moke.microfox.system;
-//
-//import ir.moke.kafir.annotation.POST;
-//
-//public interface MicroFoxAdminAPI {
-//    @POST("/api/v1/statistics")
-//    void statistics(SystemDTO dto);
-//}
